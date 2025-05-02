e1f00b8233e8b7d50d9ff0f1093d3809799acd01
Yiwei Wang <yiwei03.wang@tum.de>
2025-05-03 00:25:39 +0200

    Change the keymap for command and mapleader

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
+-- Change the mapleader to ","
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
